a91f17c39a9666979afd6a091000a4a063707aa2
android718 <100258018+android718@users.noreply.github.com>
2022-03-31 13:15:22 -0700

    final commit

--- dom-menu-lab 2/JS/script.js
+++ dom-menu-lab 2/JS/script.js
@@ -1,33 +1,42 @@
-
-
+///Goal is to pull everything through JS, NOT CSS!!
 
 
 var menuLinks = [
-  { text: "about", href: "/about" },
-  {
-    text: "catalog",
-    href: "#",
-    subLinks: [
-      { text: "all", href: "/catalog/all" },
-      { text: "top selling", href: "/catalog/top" },
-      { text: "search", href: "/catalog/search" },
-    ],
-  },
-  {
-    text: "orders",
-    href: "#",
-    subLinks: [
-      { text: "new", href: "/orders/new" },
-      { text: "pending", href: "/orders/pending" },
-      { text: "history", href: "/orders/history" },
-    ],
-  },
-  {
-    text: "account",
-    href: "#",
-    subLinks: [
-      { text: "profile", href: "/account/profile" },
-      { text: "sign out", href: "/account/signout" },
-    ],
-  },
-]
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
+//T1
+var mainEl = document.querySelector('main');
+
+//T1.1
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+//T1.2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+
+//T1.3
+mainEl.classList.add('flex-ctr');
+
+//T2
+var topMenuEl = document.getElementById('top-menu');
+
+//T2.1
+topMenuEl.style.height = '100%'
+
+//T2.2
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+//T2.2
+topMenuEl.classList.add('flex-around');
+
+//T3.1
+menuLinks.forEach(function(link) {
+  var linkEl = document.createElement('a');
+  linkEl.setAttribute('href',link.href);
+  linkEl.textContent = link.text;
+  topMenuEl.appendChild(linkEl);
+
+});
