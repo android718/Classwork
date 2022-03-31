b0dd08750c6a339677b3368fb520c37dfa023a3e
android718 <100258018+android718@users.noreply.github.com>
2022-03-30 23:41:44 -0700

    completed step 4

---
+++ arrayshw/script.js
@@ -0,0 +1,39 @@
+/*
+Exercise 1:
+  - Define an empty array named foods
+*/
+
+// Exercise 1 has been completed for you...
+
+const foods = ['taco', 'pizza','cheeseburger']
+
+console.log("Exercise 1 Result: ", foods)
+
+/*
+Exercise 2:
+  - Add the strings 'pizza' & 'cheeseburger' to the foods array such that 'pizza' comes before 'cheeseburger'.
+*/
+
+// Complete Exercise 2 below...
+
+console.log("Exercise 2 Result: ", foods)
+
+/*
+Exercise 3:
+  - Add the string 'taco' to the foods array so that 'taco' is the first food in the array.
+*/
+
+// Complete Exercise 3 below...
+
+console.log("Exercise 3 Result: ", foods)
+/*
+Exercise 4:
+  - Access the string 'pizza' (based upon its known position) in the foods array and assign to a variable named favFood.
+*/
+const favFood = ['taco', 'pizza','cheeseburger']
+// Complete Exercise 4 below...
+
+console.log("Exercise 4 Result: ", favFood[1])
+
+
+
