44ba230ac32ab9cfd659465308d932eae16954d4
android718 <100258018+android718@users.noreply.github.com>
2022-04-03 10:45:10 -0700

    attempting to display information on the dom

--- weather-app/js/app.js
+++ weather-app/js/app.js
@@ -2,15 +2,20 @@ const APIKEY="30723883f60a29ab406644e4467c8e60";
 let $searchinput = $("input")
 let $button  = $('.submit')
 
+
 $button.on('click', ()  => {
     let text =$searchinput.val()
     $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=+${text}&APPID=${APIKEY}`).then(
     (data) => { 
         console.log(data)
     })
+    let $searches = $("searches")
+    $button.on('click', ()  => {
+        let searchTerm =$searches.val()
+        
+        $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=+${searchTerm}&APPID=${APIKEY}`)
+        .then( (data) => { 
+            console.log(searchTerm)
+        })
 
-   
- 
-
-
-});
+}});
