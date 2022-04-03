cad501dc223b060337dcb501df6af1dd69c2ebaf
android718 <100258018+android718@users.noreply.github.com>
2022-04-03 14:39:58 -0700

    no errors but not displaying on screen

--- weather-app/js/app.js
+++ weather-app/js/app.js
@@ -9,6 +9,37 @@ $button.on('click', ()  => {
     (data) => { 
         console.log(data)
     })
+    let mainData, userInput
 
+    const $name = $($searchinput[type="text"])
+    const $temp = $(temp)
+    const $feels_like = $(feels_like)
+    const $weather = $(weather)
     
-})
+    $("form").on("submit", handleGetData)
+    
+    function handleGetData(event) {
+      event.preventDefault()
+      // calling preventDefault() on a 'submit' event will prevent a page refresh
+      userInput = $input.val()
+      // getting the user input
+      $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?q=+${text}&APPID=${APIKEY}`+ userInput,
+      }).then(
+        (data) => {
+          mainData = data
+          render()
+        },
+        (error) => {
+          console.log("bad request", error)
+        }
+      )
+    }})
+    
+    function render() {
+      $name.text(mainData.Name)
+      $year.text(mainData.Temp)
+      $feelsLike.text(mainData.Feels_Like)
+      $weather.text(mainData.Weather)
+    
+    ()}
